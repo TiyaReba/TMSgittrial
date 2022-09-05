394d28fc6ee03aad733529b6260ce5113db6b278
TiyaReba <105912721+TiyaReba@users.noreply.github.com>
2022-09-05 14:26:50 +0530

    changes in trainer name

--- src/app/enrollmentform/enrollmentform.component.ts
+++ src/app/enrollmentform/enrollmentform.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { DomSanitizer } from '@angular/platform-browser';
 
